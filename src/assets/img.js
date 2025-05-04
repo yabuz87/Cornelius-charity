dad5fc7ef38c6e7db45fd135be56439d8bd68a8d
Yabets solomon <jabezsolomonz@gmail.com>
2025-05-05 02:37:45 +0300

    add

--- src/assets/img.js
+++ src/assets/img.js
@@ -1,14 +1,17 @@
-import image1 from "./imgs/img0.jpg"
+import image1 from "./imgs/newImg1.jpg"
 import image2 from "./imgs/img1.jpg"
 import image3 from "./imgs/img2.jpg"
+import image4 from "./imgs/newImg2.jpg"
+import image5 from "./imgs/newImg3.jpg"
+import image6 from "./imgs/newImg4.jpg"
  const images=[
     image1,
     image2,
     image3,
-    image1,
+    image4,
+    image5,
+    image6,
+    image4,
     image2,
-    image1,
-    image2,
-    image1,
 ]
 export default images;
